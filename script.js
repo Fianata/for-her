f64f8e3297a8ff62d0c758b8156b4aba7960485b
Fianata <toryfnt2@gmail.com>
2026-01-27 13:51:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -190,7 +190,8 @@ async function lanjutKeVideo() {
 
 // --- 6. CHAT WA ---
 function chatWhatsApp() {
-    const nomorHP = "0881036799054"; // GANTI NOMOR LU
+    const nomorHP = "6281036799054"; // GANTI NOMOR LU
     const pesan = "silahkan di sampaikan apa yang mau di sampein ya mel kalo adaa :)";
     window.open(`https://wa.me/${nomorHP}?text=${encodeURIComponent(pesan)}`, '_blank');
 }
+
