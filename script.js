9c802e35d7d70ec37cdd5aab81f3f12a1b25b97b
Fianata <toryfnt2@gmail.com>
2026-09-18 05:32:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ async function lanjutKeVideo() {
 
     // Setup Voice
     if (!voiceSource) {
-        audioGain = audioCtx.createGain(); audioGain.gain.value = 2.0; 
+        audioGain = audioCtx.createGain(); audioGain.gain.value = 0.80; 
         voiceSource = audioCtx.createMediaElementSource(voice);
         voiceSource.connect(audioGain); audioGain.connect(audioCtx.destination);
     }
